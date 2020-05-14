a26d21d61e9de88b309148178df2af0bc6080c68
achugo <cachugo@infomall.ng>
2020-05-14 08:13:19 +0100

    added fonts

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,49 +1,53 @@
-import React, { useState } from 'react';
-import { Route } from 'react-router';
-import { useRouteMatch } from 'react-router-dom';
-import { Layout } from './components/Layout';
-import Home from './components/Home/Home';
-import Discover from './components/Discover/Discover';
-import Contact from './components/Contact';
-import AboutUs from './components/AboutUs';
+import React, { useState } from "react";
+import { Route } from "react-router";
+import { useRouteMatch } from "react-router-dom";
+import { Layout } from "./components/Layout";
+import Home from "./components/Home/Home";
+import Discover from "./components/Discover/Discover";
+import Contact from "./components/Contact";
+import AboutUs from "./components/AboutUs";
 
-import './custom.css'
-import MainBody from './components/MainBody/MainBody';
-import VideoPage from './components/VideoPage/VideoPage';
-import api from './sideEffects/apis/api';
+import "./custom.css";
+import "./scss/style.css";
+import MainBody from "./components/MainBody/MainBody";
+import VideoPage from "./components/VideoPage/VideoPage";
+import api from "./sideEffects/apis/api";
 
 export default function App(props) {
-    
-    return (
-        <Layout>
-            <Route exact path='/' component={Home} />
-            <Route exact path='/discover' render={() => (
-                <MainBody>
-                    <Discover />
-                </MainBody>
-                )
-            } />
-            <Route exact path='/videos'
-                render={() =>
-                    (
-                        <MainBody>
-                            <VideoPage />
-                        </MainBody>
-                    )
-                } />
-            <Route exact path='/contents/:id'
-                render={({ match }) =>
-                    (
-                        <MainBody>
-                            <VideoPage match={match} />
-                        </MainBody>
-                    )
-                } />
-            <Route exact path='/aboutUs' component={AboutUs} />
-            <Route exact path='/contact' component={Contact} />
-        </Layout>
-    );
+  return (
+    <Layout>
+      <Route exact path="/" component={Home} />
+      <Route
+        exact
+        path="/discover"
+        render={() => (
+          <MainBody>
+            <Discover />
+          </MainBody>
+        )}
+      />
+      <Route
+        exact
+        path="/videos"
+        render={() => (
+          <MainBody>
+            <VideoPage />
+          </MainBody>
+        )}
+      />
+      <Route
+        exact
+        path="/contents/:id"
+        render={({ match }) => (
+          <MainBody>
+            <VideoPage match={match} />
+          </MainBody>
+        )}
+      />
+      <Route exact path="/aboutUs" component={AboutUs} />
+      <Route exact path="/contact" component={Contact} />
+    </Layout>
+  );
 }
-
 
 App.displayName = App.name;
--- ClientApp/src/components/Home/Home.js
+++ ClientApp/src/components/Home/Home.js
@@ -1,42 +1,52 @@
-import React, { useState, useEffect } from 'react';
-import Header from '../Header/Header';
-import DropDown from '../Dropdown/DropDown';
-import PhotoGrid from '../PhotoGrid/PhotoGrid';
-import api from '../../sideEffects/apis/api';
-import MainBody from '../MainBody/MainBody';
+import React, { useState, useEffect } from "react";
+import Header from "../Header/Header";
+import DropDown from "../Dropdown/DropDown";
+import PhotoGrid from "../PhotoGrid/PhotoGrid";
+import api from "../../sideEffects/apis/api";
+import MainBody from "../MainBody/MainBody";
 
 export default function Home(props) {
-    const [contents, setContents] = useState([]);
-    const [pageLoaded, setPageLoaded] = useState(true);
+  const [contents, setContents] = useState([]);
+  const [pageLoaded, setPageLoaded] = useState(true);
 
-    useEffect(() => {
-        if (pageLoaded) {
-            api.getAll("images")
-                .then(response => {
-                    setContents(response)
-                    setPageLoaded(false)
-                })
-                .catch(err => {
-                    console.log(err)
-                    setPageLoaded(false)
-                })
-        }
-    }, [pageLoaded])
+  useEffect(() => {
+    if (pageLoaded) {
+      api
+        .getAll("images")
+        .then((response) => {
+          setContents(response);
+          setPageLoaded(false);
+        })
+        .catch((err) => {
+          console.log(err);
+          setPageLoaded(false);
+        });
+    }
+  }, [pageLoaded]);
 
-    return (
-        <>
-            <div className="header-bg pt-1h pb-1h plr-15">
-                <Header />
-            </div>
-            <MainBody>
-                <div className="enforce-mp trend-pos mt-20">
-                    <p className="app-font-mid">Free Stock Photos</p>
-                    <DropDown />
-                </div>
-                <div className="enforce-mp mt-20">
-                    <PhotoGrid contents={contents} />
-                </div>
-            </MainBody>
-        </>
-    )
+  return (
+    <>
+      <div className="homeWrapper">
+        <img
+          className="img-fluid hero"
+          src="https://images.pexels.com/photos/2867904/pexels-photo-2867904.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=250.0&w=1000"
+          alt="background-image"
+        />
+      </div>
+      <div className="discover">
+        <div className="header-bg pt-1h pb-1h plr-15">
+          <Header />
+        </div>
+        <MainBody>
+          <div className="enforce-mp trend-pos mt-20">
+            <p className="app-font-mid">Free Stock Photos</p>
+            <DropDown />
+          </div>
+          <div className="enforce-mp mt-20">
+            <PhotoGrid contents={contents} />
+          </div>
+        </MainBody>
+      </div>
+    </>
+  );
 }
